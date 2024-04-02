e839b22d826e0d4673aaf2e58324e9256b9d2429
ashish <ashish@mangoitsolutions.com>
2024-04-02 15:22:44 +0530

    update procfile

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
+from typing import Union
+
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 
 app = FastAPI()
 
-origins = ["*"]
-app.add_middleware(
- CORSMiddleware,
- allow_origins=origins,
- allow_credentials=True,
- allow_methods=["*"],
- allow_headers=["*"],
-)
 
-@app.get("/api/test")
-async def test():
- return "Hello World!"
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
-if __name__ == "__main__":
- uvicorn.run("app:app")
+    
